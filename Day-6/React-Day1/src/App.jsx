64fd4cdbaff378b2d0d50d6cc538b797b6047653
Yashika <gyashika2555@gmail.com>
2026-06-16 17:41:17 +0530

    Updated project files

--- Day-6/React-Day1/src/App.jsx
+++ Day-6/React-Day1/src/App.jsx
@@ -6,17 +6,25 @@ import heroImg from './assets/hero.png'
 import React from "react";
 import Home from "./Home";
 import Navbar from "./Navbar";
+import FormHandling from './FormHandling';
+import MultiForm from './MultiForm';
+import Demo from './Demo';
+import UserForm from './UserForm';
 // import Demo from "./Demo";
 
 const App = () => {
   return (
     <div>
       <Navbar/>
-      <div>App</div>
+      <div>App</div> 
       <h1>This is heading tag</h1>
       <Home/>
       {/* <Demo/> */}
 
+      {/* <FormHandling></FormHandling>
+      <MultiForm></MultiForm>
+      <Demo></Demo>
+      <UserForm></UserForm> */}
     </div>
   );
 };
---
+++ Day-6/React-Day1/src/FormHandling.jsx
@@ -0,0 +1,22 @@
+import React,{useState} from 'react'
+
+const FormHandling = () => {
+   const [name, setName] = useState("");
+
+   const handleChange=(e)=>{
+    console.log(e)
+    let value=e.target.value
+    let name =e.target.name
+    setName(value)
+   }
+
+  return (
+    <div>
+      <input type="text" placeholder="Enter name"
+       onChange={handleChange}
+      />
+      <h2>Hello, {name}</h2>
+    </div>
+  );
+}
+export default FormHandling
