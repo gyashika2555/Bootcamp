c9725f9cb534d152b2d5b9f67c135ae102f88fd9
Yashika <gyashika2555@gmail.com>
2026-06-30 17:55:33 +0530

    update env file

--- Backend/login-signup/project/frontend/src/api/auth.js
+++ Backend/login-signup/project/frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8888/pages';
+const BASE_URL = import.meta.env.VITE_API_URL || 'https://login-signup-e69r.onrender.com';
 
 const client = axios.create({
   baseURL: BASE_URL,
